073d04e4e2c3e2d06cadc820301ce3b9e5fb6c0b
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-04-09 10:53:33 +0600

    Update and rename ChartModel.ts to DataModel.ts

---
+++ FrontEnd/src/app/DataModel.ts
@@ -0,0 +1,12 @@
+export interface UserModel {
+    lat: number,
+    lang: number,
+    name: string,
+    fare: number
+}
+
+export interface RatingModel {
+    driverName: string,
+    riderName: string,
+    rating: number
+}
