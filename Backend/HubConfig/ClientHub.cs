882fb8ac47f0443e7bad1ddcaba6dcb6754d9c19
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-04-09 10:58:27 +0600

    Update and rename ChartHub.cs to ClientHub.cs

---
+++ Backend/HubConfig/ClientHub.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace Backend.HubConfig
+{
+    public class ClientHub : Hub
+    {
+        public void BroadcastChartData()
+        {
+        } 
+    }
+}
