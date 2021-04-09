91ca54fc9b42f68c9baa83b606aef7a29b995835
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-04-09 10:59:05 +0600

    Update and rename ChartModel.cs to DataModel.cs

--- Backend/Models/DataModel.cs
+++ Backend/Models/DataModel.cs
@@ -1,10 +1,12 @@
-namespace DS_Lab.Models
+using System.Collections.Generic;
+
+namespace Backend.Models
 {
-    public class ChartModel
+    public class DataModel
     {
         public double lat { get; set; }
         public double lang { get; set; }
         public string name { get; set; }
-        public string connectionId { get; set; }
+        public double fare { get; set; }
     }
 }
