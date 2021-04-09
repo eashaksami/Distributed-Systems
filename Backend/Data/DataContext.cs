b6b3a5ab0a9491128af33be67b42733962bca89e
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-04-09 11:01:47 +0600

    Add files via upload

---
+++ Backend/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+        
+        public DbSet<Ratings> Ratings { get; set; }
+    }
+}
---
+++ Backend/Migrations/20210408175426_InitialMigration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RealTimeCharts_Server.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ratings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DriverName = table.Column<string>(type: "TEXT", nullable: true),
+                    RiderName = table.Column<string>(type: "TEXT", nullable: true),
+                    Rating = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ratings", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ratings");
+        }
+    }
+}
