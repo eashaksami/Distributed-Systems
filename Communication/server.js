c694183bf4b15ef29fee83275d29d764e86d29a6
eashaksami <46522742+eashaksami@users.noreply.github.com>
2021-09-06 00:00:27 -0400

    Update server.js

--- Communication/server.js
+++ Communication/server.js
@@ -31,5 +31,6 @@ app.post('/communication', (req, res) => {
 
 const PORT = process.env.PORT || 3002;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT);
+// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
